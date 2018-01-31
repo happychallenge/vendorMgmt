fc83393b1c684b609b072300176d79cc211cf39a
Challenge Happy <happychallenge@outlook.com>
2018-01-31 10:59:12 +0800

    wsgi file modify

--- vendorMgmt/wsgi.py
+++ vendorMgmt/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vendorMgmt.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vendorMgmt.settings.production")
 
 application = get_wsgi_application()
